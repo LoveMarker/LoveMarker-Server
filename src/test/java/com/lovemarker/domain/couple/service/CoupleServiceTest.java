c7a43fa01f90c9f2e5f44f550096298543f52fa5
sunny <rosa7536@naver.com>
2024-05-16 16:16:12 +0900

    feat: 커플 수락 API 서비스단 테스트 작성

--- src/test/java/com/lovemarker/domain/couple/service/CoupleServiceTest.java
+++ src/test/java/com/lovemarker/domain/couple/service/CoupleServiceTest.java
@@ -7,11 +7,14 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
+import com.lovemarker.domain.couple.Couple;
 import com.lovemarker.domain.couple.InviteCodeStrategy;
+import com.lovemarker.domain.couple.fixture.CoupleFixture;
 import com.lovemarker.domain.couple.repository.CoupleRepository;
 import com.lovemarker.domain.user.User;
 import com.lovemarker.domain.user.fixture.UserFixture;
 import com.lovemarker.domain.user.repository.UserRepository;
+import com.lovemarker.global.exception.BadRequestException;
 import com.lovemarker.global.exception.NotFoundException;
 import java.time.LocalDate;
 import java.util.Optional;
@@ -67,6 +70,68 @@ class CoupleServiceTest {
 
             //then
             assertThat(exception).isInstanceOf(NotFoundException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("joinCouple 메서드 실행 시")
+    class JoinCoupleTest {
+
+        User user = UserFixture.user();
+        Couple couple = CoupleFixture.couple();
+
+        @Test
+        @DisplayName("성공")
+        void joinCouple() {
+            //given
+            given(userRepository.findById(anyLong())).willReturn(Optional.ofNullable(user));
+            given(coupleRepository.findByInviteCode(any())).willReturn(Optional.ofNullable(couple));
+            given(userRepository.countByCouple_CoupleId(any())).willReturn(1L);
+
+            //when
+            coupleService.joinCouple(1L, "inviteCode");
+
+            //then
+            assertThat(user.getCouple()).isEqualTo(couple);
+        }
+
+        @Test
+        @DisplayName("예외(NotFoundException): 존재하지 않는 유저")
+        void exceptionWhenUserNotFound() {
+            //given
+            //when
+            Exception exception = catchException(() -> coupleService.joinCouple(1L, "inviteCode"));
+
+            //then
+            assertThat(exception).isInstanceOf(NotFoundException.class);
+        }
+
+        @Test
+        @DisplayName("예외(NotFoundException): 존재하지 않는 커플")
+        void exceptionWhenCoupleNotFound() {
+            //given
+            given(userRepository.findById(anyLong())).willReturn(Optional.ofNullable(user));
+
+            //when
+            Exception exception = catchException(() -> coupleService.joinCouple(1L, "inviteCode"));
+
+            //then
+            assertThat(exception).isInstanceOf(NotFoundException.class);
+        }
+
+        @Test
+        @DisplayName("예외(BadRequestException): 이미 연결이 완료된 커플")
+        void exceptionWhenCoupleAlreadyConnected() {
+            //given
+            given(userRepository.findById(anyLong())).willReturn(Optional.ofNullable(user));
+            given(coupleRepository.findByInviteCode(any())).willReturn(Optional.ofNullable(couple));
+            given(userRepository.countByCouple_CoupleId(any())).willReturn(2L);
+
+            //when
+            Exception exception = catchException(() -> coupleService.joinCouple(1L, "inviteCode"));
+
+            //then
+            assertThat(exception).isInstanceOf(BadRequestException.class);
         }
     }
 }
