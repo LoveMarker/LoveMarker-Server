9dfdb024c3d2742b4758d4ba5099ab4d2d693527
sunny <rosa7536@naver.com>
2024-06-09 11:33:35 +0900

    test: service 테스트

--- src/main/java/com/lovemarker/domain/user/service/UserService.java
+++ src/main/java/com/lovemarker/domain/user/service/UserService.java
@@ -2,9 +2,9 @@ package com.lovemarker.domain.user.service;
 
 import com.lovemarker.domain.user.User;
 import com.lovemarker.domain.user.exception.UserNicknameDuplicateException;
+import com.lovemarker.domain.user.exception.UserNotFoundException;
 import com.lovemarker.domain.user.repository.UserRepository;
 import com.lovemarker.global.constant.ErrorCode;
-import com.lovemarker.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +23,7 @@ public class UserService {
 
     private User getUserByUserId(Long userId) {
         return userRepository.findById(userId)
-            .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_EXCEPTION, "존재하지 않는 유저입니다."));
+            .orElseThrow(() -> new UserNotFoundException(ErrorCode.NOT_FOUND_EXCEPTION, "존재하지 않는 유저입니다."));
     }
 
     private void checkDuplicatedNickname(String nickname) {
