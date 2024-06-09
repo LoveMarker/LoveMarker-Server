162dd17355dba3623c15d082f296e1ec6ca33855
sunny <rosa7536@naver.com>
2024-06-09 12:11:07 +0900

    feat: 마이페이지 조회 service

--- src/main/java/com/lovemarker/domain/user/repository/UserRepository.java
+++ src/main/java/com/lovemarker/domain/user/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.lovemarker.domain.user.User;
 import com.lovemarker.domain.user.vo.SocialType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -11,5 +13,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySocialToken_SocialTokenAndProvider(String socialToken, SocialType provider);
     boolean existsBySocialToken_SocialTokenAndProvider(String socialToken, SocialType provider);
     boolean existsByNickname_Nickname(String nickname);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.couple c "
+        + "WHERE c.coupleId = :coupleId "
+        + "AND u.userId != :userId")
+    Optional<User> findPartnerUser(@Param("coupleId") Long coupleId, @Param("userId") Long userId);
 
 }
