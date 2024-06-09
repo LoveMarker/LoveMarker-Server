d474e9c59b4feea8591c77dde9a6c23fe3bfcc6d
sunny <rosa7536@naver.com>
2024-06-09 12:17:08 +0900

    feat: 마이페이지 조회 controller

--- src/main/java/com/lovemarker/domain/user/controller/UserController.java
+++ src/main/java/com/lovemarker/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.lovemarker.domain.user.controller;
 
+import com.lovemarker.domain.user.dto.FindMyPageResponse;
 import com.lovemarker.domain.user.dto.request.UpdateUserNicknameRequest;
 import com.lovemarker.domain.user.service.UserService;
 import com.lovemarker.global.config.resolver.UserId;
@@ -7,6 +8,7 @@ import com.lovemarker.global.constant.SuccessCode;
 import com.lovemarker.global.dto.ApiResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public ApiResponseDto<FindMyPageResponse> findMyPage(
+        @UserId Long userId
+    ) {
+        return ApiResponseDto.success(SuccessCode.FIND_MY_PAGE_SUCCESS, userService.findMyPage(userId));
+    }
 
     @PatchMapping("/nickname")
     public ApiResponseDto updateUserNickname(
