4ba3e0920d28b7a7be697898f6dc755471b3aa5f
sunny <rosa7536@naver.com>
2024-11-28 16:58:00 +0900

    fix : 파일 하나만 받도록

--- src/main/java/com/lovemarker/domain/memory/dto/request/CreateMemoryRequest.java
+++ src/main/java/com/lovemarker/domain/memory/dto/request/CreateMemoryRequest.java
@@ -2,9 +2,7 @@ package com.lovemarker.domain.memory.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public record CreateMemoryRequest(
@@ -15,8 +13,7 @@ public record CreateMemoryRequest(
     @NotNull(message = "좌표는 필수 입력 항목입니다.") Double longitude,
     @NotBlank(message = "주소는 필수 입력 항목입니다.") String address,
     @NotNull(message = "이미지는 필수 입력 항목입니다.")
-    @Size(min = 1, max = 5, message = "이미지는 1개 이상 5개 이하로 선택하세요.")
-    List<MultipartFile> images
+    MultipartFile images
 ) {
 
 }
